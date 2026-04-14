32c9d842518c67042b827bfd42de8270261c594d
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-04-14 23:37:09 +0530

    full code for Simple interest

--- Java/src/test/java/JavaFirstClass/SimpleInterest.java
+++ Java/src/test/java/JavaFirstClass/SimpleInterest.java
@@ -1,5 +1,27 @@
 package JavaFirstClass;
 
+import java.util.Scanner;
+
+//Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
 public class SimpleInterest {
+	
+	public static void main(String args[])
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the Pricipal : ");
+		double P = sc.nextDouble();
+		
+		System.out.println("Enter the Rate fo Interest : ");
+		double R = sc.nextDouble();
+		
+		System.out.println("Enter the Time(In Years) : ");
+		double T = sc.nextDouble();
+		
+		double SI = (P * R * T) / 100;
+		
+		System.out.println("The Simple interest is : " + SI);
+	}
 
 }
