95b020b012e9615e6ffa32e717c4cf2628ee9b75
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-05-16 16:12:09 +0530

    Array main class

---
+++ Java/src/test/java/Array_ArrayList/Main.java
@@ -0,0 +1,31 @@
+package Array_ArrayList;
+
+public class Main {
+	
+	public static void main(String args[])
+	{
+		
+		//store a number
+		int a = 10;
+		
+		//store a name 
+		String name="Dhanoj";
+		
+		//Store 500 numbers/roll number -> not possible then array comes.
+		
+		
+		//Syntax
+		// datatype[] variable_name = new datatype[size];
+		
+		int[] rollNo = new int[5];
+		              //-----------> creating the object in heap memory.
+		int[] rollNo2 = {1,2,3,4,5};
+		
+		
+		String[] arr = new String[4];
+		System.out.println(arr[0]);       //null
+		
+		
+	}
+
+}
