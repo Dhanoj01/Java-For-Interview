4400532e076a422b6fd34f45b078aa69a3e921ed
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-05-17 17:43:24 +0530

    swap array Question

---
+++ Java/src/test/java/Array_ArrayList/Q1swap.java
@@ -0,0 +1,36 @@
+package Array_ArrayList;
+
+public class Q1swap {
+
+	public static void main(String args[])
+	{
+		int[] arr = {1,2,3,4};
+		
+		System.out.println("Array before swap......");
+		print(arr);
+		
+		swap(arr , 1 , 3);
+		
+		System.out.println("Array After swap......");
+		print(arr);
+	}
+	
+	public static void swap(int arr[] , int index1 , int index2)
+	{
+	   
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+	
+	public static void print(int arr[])
+	{
+		for(int a : arr)
+		{
+			System.out.print(a+" ");
+		}
+		System.out.println();
+
+		
+	}
+}
