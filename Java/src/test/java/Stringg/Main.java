caf4ac7fbea78ee08a2160e270d59cb02d8f1d97
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-07-12 17:02:06 +0530

    String , stack ,heap , String pool , outside of string pool

---
+++ Java/src/test/java/Stringg/Main.java
@@ -0,0 +1,20 @@
+package Stringg;
+
+public class Main {
+	
+	public static void main(String args[])
+	{
+		
+	//	String name = "Dhanoj Singh";
+		
+	//	System.out.println(name);
+		
+		String a = "Dhanoj";
+		System.out.println(a);
+		
+		a="Singh";
+		
+		System.out.println(a);
+	}
+
+}
