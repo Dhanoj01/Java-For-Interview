53ad20ba0d3722b8787e339ad1c426914c7ab049
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-07-19 16:01:45 +0530

    performance of java

---
+++ Java/src/test/java/Stringg/Performance.java
@@ -0,0 +1,20 @@
+package Stringg;
+
+public class Performance {
+	
+	public static void main(String args[])
+	{
+		
+		String series="";
+		
+		for(int i=0 ; i<26 ; i++)
+		{
+			
+			char ch = (char)('a'+i);
+		    series+=ch;
+		}
+		
+		System.out.println(series);
+	}
+
+}
